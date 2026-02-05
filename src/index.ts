62ddd85ed28f4820c2194a517bfe43d262d89c42
williamlunelli <williamlunelli07@gmail.com>
2026-02-05 09:12:23 -0300

    Feat: created basic folder structure

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import helmet from "helmet";
 import path from "path";
 
-require("dotenv").config();
+import "dotenv/config";
 
 const server = express();
 server.use(helmet());
@@ -10,12 +10,12 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(path.join(__dirname, "../public")));
 
-server.use("/", (req, res) => {
+server.get("/", (req, res) => {
   res.json("Olá mundo!");
 });
 
-server.listen(process.env.PORT, () => {
-  console.log(
-    `O servidor se encontra na porta http://localhost:${process.env.PORT}/`,
-  );
+const PORT = process.env.PORT;
+
+server.listen(Number(PORT), () => {
+  console.log(`O servidor se encontra na porta http://localhost:${PORT}/`);
 });
