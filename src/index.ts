b114c12bf45b4a052d9c894c55736ab41f3f4b74
williamlunelli <williamlunelli07@gmail.com>
2026-02-05 09:03:29 -0300

    Feat: Server initialized

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Olá mundo");
+import express from "express";
+import helmet from "helmet";
+import path from "path";
+
+require("dotenv").config();
+
+const server = express();
+server.use(helmet());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static(path.join(__dirname, "../public")));
+
+server.use("/", (req, res) => {
+  res.json("Olá mundo!");
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(
+    `O servidor se encontra na porta http://localhost:${process.env.PORT}/`,
+  );
+});
