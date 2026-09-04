aecd328005a04a0d86111888b729e762c7ffa53e
Guilherme Barbosa <guisousabarbosa06@gmail.com>
2026-09-04 11:59:22 -0300

    feat(cycles): Implementando rotas do ciclo de estudo

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import Healthrouter from "./health.routes";
 import Userrouter from "./user.routes";
 import Goalrouter from "./goal.routes";
 import Onboardingrouter from "./onboarding.routes";
+import StudyCyclerouter from "./cycle.routes";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/health", Healthrouter);
 router.use("/auth", Userrouter);
 router.use("/goals", Goalrouter);
 router.use("/me", Onboardingrouter);
+router.use("/cycles", StudyCyclerouter);
 
 export default router;
